a3293c1ab6dde1c67d8b69132a0600a7984668c4
ThomasDose <thomas.dose@me.com>
2023-02-04 21:58:26 +0530

    Rect r

--- facedetect.cpp
+++ facedetect.cpp
@@ -55,6 +55,7 @@ int main( int argc, const char** argv )
             break;
         
         imshow( "original", frame);	                                            // show original
+        
         cvtColor( frame, gray, COLOR_BGR2GRAY );                                // some preperation
         resize( gray, smallImg, Size(), 1, 1, INTER_LINEAR_EXACT );
         equalizeHist( smallImg, smallImg );
@@ -69,7 +70,7 @@ int main( int argc, const char** argv )
         
         ratio = double(frame.rows ) / double(frame.cols );                      // calculate frame ratio
         
-        Rect r = faces[0];                                                      // get data from first face
+        
         
         if (faces.size() == 0)                                                   // if no faces detected
         {
@@ -77,96 +78,99 @@ int main( int argc, const char** argv )
             r.y = 0;
             r.width  = frame.cols;                                              // full size
             r.height = cvRound(double(frame.cols) * ratio);                     // ensure same ratio
-        }
-        r.width = r.width * magify;                                             // set size of face in x
-        r.height = cvRound(double(r.width) * ratio);                            // set size of face in y
-        mov_av[counter].start.x = r.x - r.width / 2;                            // set start point of face
-        mov_av[counter].start.y = r.y - r.height / 2;
-        mov_av[counter].end.x   = r.x + r.width;                                // set size of face
-        mov_av[counter].end.y   = r.y + r.height;
-        
+        } else {
+            r = faces[0];                                                      // get data from first face
+            
+            r.width = r.width * magify;                                             // set size of face in x
+            r.height = cvRound(double(r.width) * ratio);                            // set size of face in y
+            mov_av[counter].start.x = r.x - r.width / 2;                            // set start point of face
+            mov_av[counter].start.y = r.y - r.height / 2;
+            mov_av[counter].end.x   = r.x + r.width;                                // set size of face
+            mov_av[counter].end.y   = r.y + r.height;
+            
 #ifdef DEBUG
-        cout <<                                                                 // debug
-        "x " << mov_av[counter].start.x <<
-        " y: " << mov_av[counter].start.y <<
-        " w: " << mov_av[counter].end.x <<
-        " h: " << mov_av[counter].end.y <<
-        " c: " << counter ;
+            cout <<                                                                 // debug
+            "x " << mov_av[counter].start.x <<
+            " y: " << mov_av[counter].start.y <<
+            " w: " << mov_av[counter].end.x <<
+            " h: " << mov_av[counter].end.y <<
+            " c: " << counter ;
 #endif
-        counter += 1;                                                           // increase counter by one
-        if (counter > count_mov_av) counter = 0;
-        
-        face_av.start.x = 0;
-        face_av.start.y = 0;
-        face_av.end.x   = 0;
-        face_av.end.y   = 0;
-        
-        for (int c = 0; c < count_mov_av; c++){                                 // build average
-            face_av.start.x += mov_av[c].start.x;
-            face_av.start.y += mov_av[c].start.y;
-            face_av.end.x += mov_av[c].end.x;
-            face_av.end.y += mov_av[c].end.y;
-        }
-        face_av.start.x = cvRound(double(face_av.start.x) / double(count_mov_av));
-        face_av.start.y = cvRound(double(face_av.start.y) / double(count_mov_av));
-        face_av.end.x = cvRound(double(face_av.end.x) / double(count_mov_av));
-        face_av.end.y = cvRound(double(face_av.end.y) / double(count_mov_av));
-        
-        if (face_av.start.x < 1) face_av.start.x = 0;                           // check lower values
-        if (face_av.start.y < 1) face_av.start.y = 0;                           // check lower values
-        
-        
-        // under construction
-        // frame gets squeezed at the borders
-        // the size must be changed to fit into frame
-        /*
-        if  ((face_av.end.x - face_av.start.x > frame.cols) ||
-            (face_av.end.y - face_av.start.y > frame.rows))
-        {
-            break;
-        }
-        */
-        
-        
-        if (face_av.end.x > frame.cols)                                         // check upper values
-        {
-            face_av.end.x = frame.cols;                                         // end is not bigger than frame end
-        }
-        if (face_av.end.y > frame.rows)                                         // check upper values
-        {
-            face_av.end.y = frame.rows;
-        }
-        
-        
+            counter += 1;                                                           // increase counter by one
+            if (counter > count_mov_av) counter = 0;
+            
+            face_av.start.x = 0;
+            face_av.start.y = 0;
+            face_av.end.x   = 0;
+            face_av.end.y   = 0;
+            
+            for (int c = 0; c < count_mov_av; c++){                                 // build average
+                face_av.start.x += mov_av[c].start.x;
+                face_av.start.y += mov_av[c].start.y;
+                face_av.end.x += mov_av[c].end.x;
+                face_av.end.y += mov_av[c].end.y;
+            }
+            face_av.start.x = cvRound(double(face_av.start.x) / double(count_mov_av));
+            face_av.start.y = cvRound(double(face_av.start.y) / double(count_mov_av));
+            face_av.end.x = cvRound(double(face_av.end.x) / double(count_mov_av));
+            face_av.end.y = cvRound(double(face_av.end.y) / double(count_mov_av));
+            
+            if (face_av.start.x < 1) face_av.start.x = 0;                           // check lower values
+            if (face_av.start.y < 1) face_av.start.y = 0;                           // check lower values
+            
+            
+            // under construction
+            // frame gets squeezed at the borders
+            // the size must be changed to fit into frame
+            /*
+             if  ((face_av.end.x - face_av.start.x > frame.cols) ||
+             (face_av.end.y - face_av.start.y > frame.rows))
+             {
+             break;
+             }
+             */
+            
+            
+            if (face_av.end.x > frame.cols)                                         // check upper values
+            {
+                face_av.end.x = frame.cols;                                         // end is not bigger than frame end
+            }
+            if (face_av.end.y > frame.rows)                                         // check upper values
+            {
+                face_av.end.y = frame.rows;
+            }
+            
+            
 #ifdef DEBUG
-        cout << "  start" << face_av.start.x << " END: "<< face_av.end.x <<  endl;
+            cout << "  start" << face_av.start.x << " END: "<< face_av.end.x <<  endl;
 #endif
-        
-        
-        
-        frame_cropped=frame(cv::Rect(face_av.start.x,                           // crop the video
-                                     face_av.start.y,
-                                     face_av.end.x - face_av.start.x,
-                                     face_av.end.y - face_av.start.y));
-        
-        resize(frame_cropped,                                                   // resize the video
-               frame_show,
-               Size(frame.cols,frame.rows),
-               1, 1 ,INTER_LINEAR_EXACT);
-        
-        
-        
-        imshow( "detect", frame_show );	                                        // show face
-        
-        t = (double)getTickCount() - t;                                         // get new time
-        
+            
+            
+            
+            frame_cropped=frame(cv::Rect(face_av.start.x,                           // crop the video
+                                         face_av.start.y,
+                                         face_av.end.x - face_av.start.x,
+                                         face_av.end.y - face_av.start.y));
+            
+            resize(frame_cropped,                                                   // resize the video
+                   frame_show,
+                   Size(frame.cols,frame.rows),
+                   1, 1 ,INTER_LINEAR_EXACT);
+            
+            
+            
+            imshow( "detect", frame_show );	                                        // show face
+            
+            t = (double)getTickCount() - t;                                         // get new time
+            
 #ifdef DEBUG
-        printf( "speed = %g fps\n", 1/t*getTickFrequency());                    // show fps
+            printf( "speed = %g fps\n", 1/t*getTickFrequency());                    // show fps
 #endif
-        char c = (char)waitKey(1);                                              // key for ending
-        if( c == 27 || c == 'q' || c == 'Q' )
-            break;
-        
+            char c = (char)waitKey(1);                                              // key for ending
+            if( c == 27 || c == 'q' || c == 'Q' )
+                break;
+            
+        }
     }
     return 0;
 }
